0d424731645a941425be5124c4a716d6c19549a3
Christopher David <chris@openagents.com>
2024-12-24 20:36:33 -0600

    Fix lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,24 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  {
+    ignores: ["**/dist/**", "**/node_modules/**"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin
+    }
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommended
 ];
