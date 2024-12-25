8ddb5d1459059aa9da2c5691321feea087be632d
Christopher David <chris@openagents.com>
2024-12-24 21:17:29 -0600

    Refactor port parsing to ensure it's an integer in app.ts for robustness.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express"
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT || '3000', 10);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
